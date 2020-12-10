7adfcde4860f7245ef989e883417592a7f071448
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2020-12-09 21:54:40 -0800

    day3 pt1 completed

---
+++ src_python/day3.py
@@ -0,0 +1,56 @@
+import time
+
+TREE = "#"
+OPEN = "."
+
+class Position:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y 
+
+def parse_input():
+    data = []
+    with open("day3.input", "r") as f:
+        for line in f.readlines():
+            data.append(line.replace("\n", "")) # strings are pointers. to speed this up, make data a single array.
+    return data;
+
+def part1():
+    results = {"trees": 0, "non-trees": 0}
+    map_data = parse_input();
+    (width, height) = len(map_data[0]), len(map_data)
+    
+    slope = Position(3, 1) # right 3, down 1
+    position = Position(0, 0)
+    while position.y < height:
+        if map_data[position.y][position.x % width] == TREE:
+            results["trees"] += 1
+        else:
+            results["non-trees"] += 1
+
+        # move the tobogan
+        position.x += slope.x
+        position.y += slope.y
+
+    print(results)
+
+
+def part2():
+    results = {"valid_password_count": 0, "invalid_password_count": 0}
+    
+    data = parse_input();
+
+    print(results)
+
+if __name__ == "__main__":
+    start = time.time()
+
+    print("starting day3 part1")
+    part1()
+    print("")
+
+    #print("starting day3 part2")
+    #part2()
+    #print("")
+
+    print("time elapsed: {}s".format(time.time() - start))
